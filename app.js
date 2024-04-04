758ddc83657ae999a10c38f98416cf41f1809b75
Pawan <titan@LAPTOP-7CDBL9O0>
2024-04-04 21:59:34 +0545

    health check added

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
