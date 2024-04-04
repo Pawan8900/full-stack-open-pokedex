9ff51d9bffc7561a46768c45a31d1a821fac592e
Pawan <titan@LAPTOP-7CDBL9O0>
2024-04-04 17:43:26 +0545

    bug fixed

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const cors = require('cors')
 
-app.use(cors())
+
 const app = express()
 
 // get the port from env variable
 const PORT = process.env.PORT || 5000
 
+app.use(cors())
 app.use(express.static('dist'))
 
 app.listen(PORT, () => {
